bf0b1091a3648632d09e4cf31f8dfcff97e8eb21
chikwongyip <527439024@qq.com>
2025-06-30 23:09:07 +0800

    get company data

--- backend/models/company.py
+++ backend/models/company.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel, HttpUrl, Field
+from pydantic import BaseModel, HttpUrl, Field, ConfigDict, field_validator
 from typing import Optional
+from datetime import datetime
 
 
 class CompanyInfo(BaseModel):
@@ -8,4 +9,13 @@ class CompanyInfo(BaseModel):
     description: Optional[str] = None
     logo_url: Optional[HttpUrl] = None
     icp_number: Optional[str] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
     # 自定义校验：确保名称有效
+    model_config = ConfigDict(from_attributes=True)
+
+    @field_validator("created_at", mode="before")
+    def parse_datetime(cls, value):
+        if isinstance(value, str):
+            return datetime.fromisoformat(value)
+        return value
--- backend/routers/company.py
+++ backend/routers/company.py
@@ -12,25 +12,27 @@ async def get_company_info(id: int):
         db_company = session.query(CompanyInfoSchema).filter_by(
             id=id).first()
     if db_company:
-        print(db_company.id)
-        return BaseResponse.success(data=db_company)
+        res = CompanyInfoModel.model_validate(db_company)
+        # print(db_company.id)
+        return BaseResponse.success(data=res)
     else:
         return BaseResponse.error(code=1, message="数据不存在")
 
 
 @router.post('/edit')
 async def edit_company_info(company: CompanyInfoModel):
-    with get_db as session:
+    print(company.id)
+    with get_db() as session:
         db_company = session.query(CompanyInfoSchema).filter_by(
             id=company.id
-        ).first()
-        if db_company:
-            db_company.name = company.name
-            db_company.description = company.description
-            db_company.logo_url = company.logo_url
-            db_company.icp_number = company.icp_number
-            session.update(db_company)
-            session.commit
-            return BaseResponse.success(data={"res": "更新成功"})
-        else:
-            return BaseResponse.error(code=1, message="comany id 不存在")
+        ).one()
+    if db_company:
+        db_company.name = company.name
+        db_company.description = company.description
+        db_company.logo_url = company.logo_url
+        db_company.icp_number = company.icp_number
+
+        session.commit()
+        return BaseResponse.success(data={"result": "更新成功"})
+    else:
+        return BaseResponse.error(code=1, message="comany id 不存在")
