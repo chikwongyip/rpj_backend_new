78a9225d71e1165e7e28a268503e0b04cec14f82
chikwong <527439024@qq.com>
2025-08-27 17:11:52 +0800

    oss

--- backend/models/company.py
+++ backend/models/company.py
@@ -9,14 +9,8 @@ class CompanyInfo(BaseModel):
     name: str = Field(..., min_length=2, max_length=20)
     description: Optional[str] = Field(
         None, title="Company Description", max_length=255)
-    logo_url: Optional[bytes] = File()
+    logo_url: Optional[bytes] = File(None)
     icp_number: Optional[str] = Field(None, max_length=100)
     created_at: Optional[datetime] = Field(default_factory=datetime.now)
     updated_at: Optional[datetime] = Field(default_factory=datetime.now)
     model_config = ConfigDict(from_attributes=True)
-
-    # @field_validator("created_at", mode="before")
-    # def parse_datetime(cls, value):
-    #     if isinstance(value, str):
-    #         return datetime.fromisoformat(value)
-    #     return value
